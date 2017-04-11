62c5b5b184b3f34202ba8f0ca88f647b6c9a8fd6
Jack Linton <jacklinton@mac.com>
2017-04-11 13:27:50 -0400

    Completed unblock and defer sections.

---
+++ bullet-unblock-master/server/geo.js
@@ -0,0 +1,10 @@
+Meteor.methods({
+  trackLocation: function() {
+  	this.unblock();
+    // assume this is a legitimate method which will
+    // invoke an API call and get more details about the 
+    // Geo location from IP
+    // We will need this on every page load
+    Meteor._sleepForMs(2000);
+  }
+});
